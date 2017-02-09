14e7cee669f9794f193a5490ce1830911617ffee
ytyaru <ytyaru@outlook.jp>
2017-02-09 09:19:49 +0900

    コピペミス修正。重複テストケース削除。

--- TestFileSize_img_fig.py
+++ TestFileSize_img_fig.py
@@ -87,48 +87,6 @@ class TestFileSize_img_fig(unittest.TestCase):
         actual = (unit ** 1)
         self.assertEqual(self.__target.Get(actual), "1 KiB")
 
-    def test_10_000KiB_4_3_zero(self):
-        unit=1024; int_fig=4; img_fig=3; zero=False;
-        self.__target = FileSize.FileSize(byte_size_of_unit=unit, integral_figure_num=int_fig, imaginary_figure_num=img_fig, is_hidden_imaginary_all_zero=zero)
-        actual = ((unit ** 1) * (10))
-        self.assertEqual(self.__target.Get(actual), "10.000 KiB")
-    def test_10_00KiB_4_2_zero(self):
-        unit=1024; int_fig=4; img_fig=2; zero=False;
-        self.__target = FileSize.FileSize(byte_size_of_unit=unit, integral_figure_num=int_fig, imaginary_figure_num=img_fig, is_hidden_imaginary_all_zero=zero)
-        actual = ((unit ** 1) * (10))
-        self.assertEqual(self.__target.Get(actual), "10.00 KiB")
-    def test_10_0KiB_4_1_zero(self):
-        unit=1024; int_fig=4; img_fig=1; zero=False;
-        self.__target = FileSize.FileSize(byte_size_of_unit=unit, integral_figure_num=int_fig, imaginary_figure_num=img_fig, is_hidden_imaginary_all_zero=zero)
-        actual = ((unit ** 1) * (10))
-        self.assertEqual(self.__target.Get(actual), "10.0 KiB")
-    def test_10KiB_4_0_zero(self):
-        unit=1024; int_fig=4; img_fig=0; zero=False;
-        self.__target = FileSize.FileSize(byte_size_of_unit=unit, integral_figure_num=int_fig, imaginary_figure_num=img_fig, is_hidden_imaginary_all_zero=zero)
-        actual = ((unit ** 1) * (10))
-        self.assertEqual(self.__target.Get(actual), "10 KiB")
-
-    def test_1_000KiB_3_3_zero(self):
-        unit=1024; int_fig=3; img_fig=3; zero=False;
-        self.__target = FileSize.FileSize(byte_size_of_unit=unit, integral_figure_num=int_fig, imaginary_figure_num=img_fig, is_hidden_imaginary_all_zero=zero)
-        actual = (unit ** 1)
-        self.assertEqual(self.__target.Get(actual), "1.000 KiB")
-    def test_1_00KiB_3_2_zero(self):
-        unit=1024; int_fig=3; img_fig=2; zero=False;
-        self.__target = FileSize.FileSize(byte_size_of_unit=unit, integral_figure_num=int_fig, imaginary_figure_num=img_fig, is_hidden_imaginary_all_zero=zero)
-        actual = (unit ** 1)
-        self.assertEqual(self.__target.Get(actual), "1.00 KiB")
-    def test_1_0KiB_3_1_zero(self):
-        unit=1024; int_fig=3; img_fig=1; zero=False;
-        self.__target = FileSize.FileSize(byte_size_of_unit=unit, integral_figure_num=int_fig, imaginary_figure_num=img_fig, is_hidden_imaginary_all_zero=zero)
-        actual = (unit ** 1)
-        self.assertEqual(self.__target.Get(actual), "1.0 KiB")
-    def test_1KiB_3_0_zero(self):
-        unit=1024; int_fig=3; img_fig=0; zero=False;
-        self.__target = FileSize.FileSize(byte_size_of_unit=unit, integral_figure_num=int_fig, imaginary_figure_num=img_fig, is_hidden_imaginary_all_zero=zero)
-        actual = (unit ** 1)
-        self.assertEqual(self.__target.Get(actual), "1 KiB")
-
     def test_10KiB_4_3(self):
         unit=1024; int_fig=4; img_fig=3; zero=True;
         self.__target = FileSize.FileSize(byte_size_of_unit=unit, integral_figure_num=int_fig, imaginary_figure_num=img_fig, is_hidden_imaginary_all_zero=zero)
